1b6ea8c6b2af5ee8816842de21405dc2bfd81349
Nathan Edwards <nathanedwardsuk@gmail.com>
2025-03-26 14:37:21 +0100

    forgot to add files in previous commit. Added rat page.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/dev-portfolio-0.0.1-SNAPSHOT.jar
--- src/main/java/portfolio/dev_portfolio/web/webConfig.java
+++ src/main/java/portfolio/dev_portfolio/web/webConfig.java
@@ -10,5 +10,6 @@ public class webConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+        registry.addViewController("/rat").setViewName("rat");
     }
 }
